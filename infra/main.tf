228b63bb3aaf9b4e8265ed309a47cd028a80b231
Marquito94 <71236169+Marquito94@users.noreply.github.com>
2025-08-09 20:23:34 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -133,26 +133,3 @@ resource "google_compute_global_forwarding_rule" "http_rule" {
   load_balancing_scheme = "EXTERNAL"
 }
 
-############################################
-# Outputs (handy in a single file)
-############################################
-
-output "service_account_email" {
-  value       = google_service_account.deployer.email
-  description = "Deployer Service Account email"
-}
-
-output "bucket_name" {
-  value       = google_storage_bucket.site.name
-  description = "Private website bucket name"
-}
-
-output "lb_ip" {
-  value       = google_compute_global_address.ip.address
-  description = "Create an A record for var.domain pointing to this IP"
-}
-
-output "https_url" {
-  value       = "https://${var.domain}"
-  description = "Site URL via HTTPS Load Balancer (after DNS + cert ACTIVE)"
-}
