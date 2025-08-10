8096c018459771ded38ce3797962075e23320e4a
Marquito94 <71236169+Marquito94@users.noreply.github.com>
2025-08-10 01:26:06 -0300

    Create dns.tf

---
+++ infra/dns.tf
@@ -0,0 +1,42 @@
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+}
+
+variable "vpc_self_link" {
+  type        = string
+  description = "Self link of the VPC network to attach the private zone to"
+  default     = "projects/app-dev-468521/global/networks/default"
+}
+
+# Private suffix (must end with a dot)
+# Example: "internal.apipueba-web-dev.com."
+variable "private_dns_name" {
+  type        = string
+  description = "Private zone DNS name (with trailing dot)"
+  default     = "internal.apipueba-web-dev.com."
+}
+
+variable "private_zone_name" {
+  type        = string
+  description = "Terraform name for the private zone"
+  default     = "private-internal-zone"
+}
+
+resource "google_dns_managed_zone" "private_zone" {
+  project     = var.project_id
+  name        = var.private_zone_name
+  dns_name    = var.private_dns_name
+  description = "Private zone for internal-only resolution"
+  visibility  = "private"
+
+  private_visibility_config {
+    networks {
+      network_url = var.vpc_self_link
+    }
+  }
+}
+
+output "private_zone_dns_name" {
+  value = google_dns_managed_zone.private_zone.dns_name
+}
