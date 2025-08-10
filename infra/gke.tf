bbc9d54286759087cf3e8c18bf960dc8e3d9e6b9
Marquito94 <71236169+Marquito94@users.noreply.github.com>
2025-08-09 22:13:39 -0300

    Create gke.tf

---
+++ infra/gke.tf
@@ -0,0 +1,68 @@
+# Enable API (safe to keep even if enabled elsewhere)
+resource "google_project_service" "container" {
+  project = var.project_id
+  service = "container.googleapis.com"
+}
+
+# Regional Standard cluster (uses var.region)
+resource "google_container_cluster" "gke" {
+  name     = var.cluster_name
+  location = var.region
+
+  networking_mode = "VPC_NATIVE"
+  ip_allocation_policy {}
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  # We'll attach our own managed node pool
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  depends_on = [google_project_service.container]
+}
+
+resource "google_container_node_pool" "primary_pool" {
+  name     = "primary-pool"
+  cluster  = google_container_cluster.gke.name
+  location = var.region
+
+  initial_node_count = 3
+
+  autoscaling {
+    min_node_count = 3
+    max_node_count = 6
+  }
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    machine_type   = var.node_machine_type          # default "e2-standard-4"
+    service_account = data.google_compute_default_service_account.default.email
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+
+    # Basic hardening
+    metadata = {
+      "disable-legacy-endpoints" = "true"
+    }
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
+    shielded_instance_config {
+      enable_secure_boot          = true
+      enable_integrity_monitoring = true
+    }
+  }
+
+  depends_on = [
+    google_container_cluster.gke,
+    google_project_iam_member.nodes_ar_reader
+  ]
+}
