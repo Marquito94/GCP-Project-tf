67f610f99cdcadad10e013d6124b1d33bc58097c
Marquito94 <71236169+Marquito94@users.noreply.github.com>
2025-08-10 03:16:15 -0300

    Update versions.tf

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,10 +1,13 @@
 terraform {
-  required_version = ">= 1.5.0"
-
+  required_version = ">= 1.5"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.40"
+      version = "~> 5.30"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.30"
     }
   }
 }
