c819f4768b5fea1775f1cd4a5c00e49a1afecf0c
Marquito94 <71236169+Marquito94@users.noreply.github.com>
2025-08-11 18:21:53 -0300

    Update apis.tf

--- infra/apis.tf
+++ infra/apis.tf
@@ -1,1 +1,29 @@
+resource "google_project_service" "enable_cloudresourcemanager" {
+  project = var.project_id
+  service = "cloudresourcemanager.googleapis.com"
+}
 
+resource "google_project_service" "enable_iam" {
+  project = var.project_id
+  service = "iam.googleapis.com"
+}
+
+resource "google_project_service" "enable_compute" {
+  project = var.project_id
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "enable_storage" {
+  project = var.project_id
+  service = "storage.googleapis.com"
+}
+
+resource "google_project_service" "servicenetworking" {
+  project = var.project_id
+  service = "servicenetworking.googleapis.com"
+}
+
+resource "google_project_service" "apigee" {
+  project = var.project_id
+  service = "apigee.googleapis.com"
+}
